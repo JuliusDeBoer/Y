df2b24a6a7da7107c28ff8dc2ab30c788f6c3f6e
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-05-19 00:32:58 +0200

    feat: Update 404/500 pages

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,13 +1,14 @@
 import { Button, Typography } from "@mui/material";
 import { Link } from "@tanstack/react-router";
+import notFoundImage from "@/assets/404 NotFound.png";
 
 export default function NotFound() {
   return (
     <div className="h-full flex flex-col justify-center items-center">
-      <Typography variant="h1">404</Typography>
-      <Typography variant="subtitle1">Not Found</Typography>
+			<img src={notFoundImage} alt="400 Not Found" width="500" />
+			<Typography variant="h5" component="h1">Oops! We didnt find what you where looking for!</Typography>
       <Link to="/">
-        <Button>Home</Button>
+        <Button>Return home</Button>
       </Link>
     </div>
   );
