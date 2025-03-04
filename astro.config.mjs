81ed6cc41d32f591ae8a605643e7a355dd8f0838
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2025-03-04 08:46:25 +0100

    fix: Maybe this will help

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,17 +28,6 @@ export default defineConfig({
             "react-dom/server": "react-dom/server.edge",
           },
         },
-    ssr: {
-      external: [
-        "path",
-        "fs",
-        "os",
-        "child_process",
-        "crypto",
-        "stream",
-        "assert",
-      ],
-    },
   },
 
   integrations: [react(), db()],
